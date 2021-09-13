bb83d8eb68a1ed66ebc5db58a339d0423b8a85cc
hyun <coderhyun476@gmail.com>
2021-09-13 21:47:27 +0900

    Create Routing agreement-location, privacy-policy

--- src/components/navigations/Main/index.tsx
+++ src/components/navigations/Main/index.tsx
@@ -6,6 +6,7 @@ import {
 
 import Agreement from "../../pages/Agreement";
 import AgreementLocation from "../../pages/AgreementLocation";
+import PrivacyPolicy from "../../pages/PrivacyPolicy";
 import React from 'react'
 
 const Main = () => {
@@ -15,7 +16,7 @@ const Main = () => {
                 <Route path='/' exact />
                 <Route path='/agreement' component={Agreement} />
                 <Route path='/agreement-location' component={AgreementLocation} />
-                <Route path='/privacy-policy' />
+                <Route path='/privacy-policy' component={PrivacyPolicy} />
                 <Route path='/support' />
             </Switch>
         </BrowserRouter>
